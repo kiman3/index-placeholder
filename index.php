df6e73c82440b61028b9c81fd5bc903d9bec1a20
kiman3 <kim.andre.ottesen@gmail.com>
2019-12-16 16:37:26 +0100

    Beginning of new text based layout

--- index.php
+++ index.php
@@ -3,11 +3,9 @@ $title = "Kim Andre Ottesen – Designer and coder at Snøhetta";
 $id = "index";
 include("header.php");
 ?>
-
-<div class="about">
-	<p class="description"><span class="title">Designer and coder</span> <br>at Snøhetta</p>
-	<p class="contact"><a href="mailto:kiman@kimandreottesen.no">Email</a></p>
-	<p class="twitter"><a href="https://twitter.com/kiman">Twitter</a></p>
-</div>
+	<div class="content">
+		<h1 class="name">Kim Andre Ottesen.</h1> <p class="description"><span class="title">Designer and developer</span> at Snøhetta. Having worked with projects and clients like <a href="#" target="_blank" rel="noopener referrer">Under</a>, <a href="#" target="_blank" rel="noopener referrer">Vestre</a>, <a href="#" target="_blank" rel="noopener referrer">New Norwegian Banknotes</a>, <a href="#" target="_blank" rel="noopener referrer">YME Universe</a>, <a href="#" target="_blank" rel="noopener referrer">The Norwegian Soloist's Choir</a>, <a href="#" target="_blank" rel="noopener referrer">Cinemateket</a>, <a href="#" target="_blank" rel="noopener referrer">Munchmuseet</a>, <a href="#" target="_blank" rel="noopener referrer">FC Oslo</a>, <a href="#" target="_blank" rel="noopener referrer">Living the Nordic Light</a>, <a href="#" target="_blank" rel="noopener referrer">Dramaten</a>, <a href="#" target="_blank" rel="noopener referrer">Bjarne Melgaard Enterprise</a> and many more. Examining the Graphic Design Bachelor of Arts and guest teaching at Westerdals Oslo ACT. Board member of Grafill Interactive, arranging talks and workshops on everything touching the digital domain of design.</p>
+		<p class="contact">Please get in touch via <a href="mailto:kiman@kimandreottesen.no">email</a> or&nbsp;<a href="https://twitter.com/kiman">Twitter</a>.</p>
+	</div>
 
 <?php include("footer.php"); ?>
