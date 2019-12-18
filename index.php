ff3717318f1229c414c30ea7e1ffe66567f56920
kiman3 <kim.andre.ottesen@gmail.com>
2019-12-18 14:11:29 +0100

    Added projects, adjusted font size

--- index.php
+++ index.php
@@ -6,17 +6,19 @@ include("header.php");
 	<div class="content">
 		<h1 class="name">Kim Andre Ottesen.</h1>
 		<p class="description"><span class="title">Designer and developer</span> at Snøhetta.</p>
-		<p class="projects">Recent work: 
+		<p class="projects">Some more or less recent work: 
 			<a href="https://snohetta.com/projects/428-under-europes-first-underwater-restaurant" target="_blank" rel="noopener referrer">Web for Under</a>, 
 			<a href="https://snohetta.com/projects/471-vestre-a-website-helping-to-create-social-meeting-spaces" target="_blank" rel="noopener referrer">web for Vestre</a>, 
-			<a href="https://snohetta.com/projects/383-yme-digital-retail-experience-and-visual-identity" target="_blank" rel="noopener referrer">web and identity for YME Universe</a>, 
-			<a href="http://northof.no/" target="_blank" rel="noopener referrer">web and identity for North Of</a>, 
+			<a href="https://snohetta.com/projects/219-melgaard-munch" target="_blank" rel="noopener referrer">exhibition design and books for The Munch Museum</a>, 
 			<a href="https://snohetta.com/projects/434-the-norwegian-soloists39-choir-visual-identity-and-website" target="_blank" rel="noopener referrer">web and identity for The Norwegian Soloist's Choir</a>, 
+			<a href="https://snohetta.com/projects/383-yme-digital-retail-experience-and-visual-identity" target="_blank" rel="noopener referrer">web and identity for YME Universe</a>, 
 			<a href="https://snohetta.com/projects/387-cinemateket" target="_blank" rel="noopener referrer">web and identity for Cinemateket</a>, 
-			<a href="https://snohetta.com/projects/219-melgaard-munch" target="_blank" rel="noopener referrer">exhibition design and books for Munchmuseet</a>, 
 			<a href="https://www.instagram.com/fcoslo.no/" target="_blank" rel="noopener referrer">idenitity and kit for FC Oslo</a>, 
+			<a href="https://www.dramaten.se/" target="_blank" rel="noopener referrer">web for Dramaten</a>, 
+			<a href="http://northof.no/" target="_blank" rel="noopener referrer">web and identity for North Of</a>, 
+			<a href="https://snohetta.com/projects/226-norway39s-national-parks" target="_blank" rel="noopener referrer">web and identity for The Norwegian National Parks</a>, 
 			<a href="https://snohetta.com/project/195-living-the-nordic-light" target="_blank" rel="noopener referrer">web for Living the Nordic Light</a>, 
-			<a href="https://www.dramaten.se/" target="_blank" rel="noopener referrer">web for Dramaten</a> 
+			<a href="https://snohetta.com/projects/438-calgary-library-signage-and-wayfinding" target="_blank" rel="noopener referrer">wayfinding and signage for Calgary Central Library</a> 
 			and 
 			<a href="https://bjarnemelgaard.enterprises/" target="_blank" rel="noopener referrer">identity for Bjarne Melgaard Enterprise</a>.
 		</p>
