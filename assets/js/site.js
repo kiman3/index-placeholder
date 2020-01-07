7d3cdce734017c4c59f8b6bf6094a0a6b1409655
kiman3 <kim.andre.ottesen@gmail.com>
2020-01-07 14:26:58 +0100

    Rewrote JS to vanilla, removed jQuery and unused JS files. Removed unused SCSS, simplified folder structure.

--- assets/js/site.js
+++ assets/js/site.js
@@ -1,20 +1,16 @@
-(function($){
-
-	var titles = ['Designer and coder', 'Graphic Designer', 'Webdesigner', 'Interactive Designer', 'Brand Designer', 'Chief Technology Officer', 'Story Designer', 'Business Designer', 'Experience Designer', 'Digital Visionary', 'Internaut', 'Webmaster', 'Change Agent', 'Front-end Developer', 'Chief Design Technologist', 'Executive Chief Technology Director', 'Art Director', 'Comedian', 'Multimedia Designer', 'Interaction Designer', 'Lead Visual Brand Designer', 'Associate Creative Director', 'Operator', 'Capable user of the Internet', 'Forecaster', 'Super Admin', 'Visual Designer', 'Mac Designer', 'Creative', 'Design critic', 'UI Design Consultant', 'Web/Transdisciplinary Designer', 'Senior Digital Innovation Strategist & UX Design Lead', 'Chief Internet Evangelist'],
-		titlesLength = titles.length -1;
+var titles = ['Designer and coder', 'Graphic Designer', 'Webdesigner', 'Interactive Designer', 'Brand Designer', 'Chief Technology Officer', 'Story Designer', 'Business Designer', 'Experience Designer', 'Digital Visionary', 'Internaut', 'Webmaster', 'Change Agent', 'Front-end Developer', 'Chief Design Technologist', 'Executive Chief Technology Director', 'Art Director', 'Comedian', 'Multimedia Designer', 'Interaction Designer', 'Lead Visual Brand Designer', 'Associate Creative Director', 'Operator', 'Capable user of the Internet', 'Forecaster', 'Super Admin', 'Visual Designer', 'Mac Designer', 'Creative', 'Design critic', 'UI Design Consultant', 'Web/Transdisciplinary Designer', 'Senior Digital Innovation Strategist & UX Design Lead', 'Chief Internet Evangelist'],
+	titlesLength = titles.length -1;
 	
-	function titleShift(){
-		i = 0;
-		$('.title').on('click', function(){
-			i++;
-			if(i <= titlesLength){
-				$(this).text(titles[i]);
-			} else{
-				$(this).text(titles[0]);
-				i = 0;
-			}
-		});
-	}
-	titleShift();
-
-})(jQuery);
+function titleShift(){
+	i = 0;
+	document.querySelector('.title').addEventListener("click", (e) => {
+		i++;
+		if(i <= titlesLength){
+			document.querySelector('.title').textContent = titles[i];
+		} else{
+			document.querySelector('.title').textContent = titles[0];
+			i = 0;
+		}
+	});
+}
+titleShift();
--- footer.php
+++ footer.php
@@ -1,10 +1,9 @@
-	<footer class="footer-main row"></footer>
+
 
 </div><!-- .site -->
 <script>
 	document.body.className = document.body.className.replace("no-js","js");
 </script>
-<script src="assets/js/jquery.min.js"></script>
 <script src="assets/js/site.js"></script>
 </body>
 </html>
