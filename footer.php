62b9013dab115f64237e11e0a868689ebbc4fce3
kiman3 <kim.andre.ottesen@gmail.com>
2020-01-07 14:39:10 +0100

    Changed to minified js

--- footer.php
+++ footer.php
@@ -4,6 +4,6 @@
 <script>
 	document.body.className = document.body.className.replace("no-js","js");
 </script>
-<script src="assets/js/site.js"></script>
+<script src="assets/js/site-min.js"></script>
 </body>
 </html>
