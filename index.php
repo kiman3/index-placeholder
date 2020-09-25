ee89b644c357c75ebdcef9203fd0d7fd466ab9ed
kiman3 <kim.andre.ottesen@gmail.com>
2020-09-25 13:47:14 +0200

    Div changes

--- index.php
+++ index.php
@@ -7,6 +7,7 @@ include("header.php");
 		<h1 class="name">Kim Andre Ottesen.</h1>
 		<p class="description"><span class="title">Designer and developer</span> at Snøhetta.</p>
 		<p class="projects">Some more or less recent work: 
+			<a href="https://snohetta.com/projects/504-bjarne-melgaard-enterprise-an-e-mail-first-visual-identity" target="_blank" rel="noopener referrer">Visual identity for Bjarne Melgaard Enterprise</a>, 
 			<a href="https://snohetta.com/projects/428-under-europes-first-underwater-restaurant" target="_blank" rel="noopener referrer">Web for Under</a>, 
 			<a href="https://snohetta.com/projects/471-vestre-a-website-helping-to-create-social-meeting-spaces" target="_blank" rel="noopener referrer">web for Vestre</a>, 
 			<a href="https://snohetta.com/projects/219-melgaard-munch" target="_blank" rel="noopener referrer">exhibition design and books for The Munch Museum</a>, 
@@ -17,10 +18,8 @@ include("header.php");
 			<a href="https://www.dramaten.se/" target="_blank" rel="noopener referrer">web for Dramaten</a>, 
 			<a href="http://northof.no/" target="_blank" rel="noopener referrer">web and identity for North Of</a>, 
 			<a href="https://snohetta.com/projects/226-norway39s-national-parks" target="_blank" rel="noopener referrer">web and identity for The Norwegian National Parks</a>, 
-			<a href="https://snohetta.com/project/195-living-the-nordic-light" target="_blank" rel="noopener referrer">web for Living the Nordic Light</a>, 
-			<a href="https://snohetta.com/projects/438-calgary-library-signage-and-wayfinding" target="_blank" rel="noopener referrer">wayfinding and signage for Calgary Central Library</a> 
-			and 
-			<a href="https://bjarnemelgaard.enterprises/" target="_blank" rel="noopener referrer">identity for Bjarne Melgaard Enterprise</a>.
+			<a href="https://snohetta.com/project/195-living-the-nordic-light" target="_blank" rel="noopener referrer">web for Living the Nordic Light</a> and 
+			<a href="https://snohetta.com/projects/438-calgary-library-signage-and-wayfinding" target="_blank" rel="noopener referrer">wayfinding and signage for Calgary Central Library</a>.
 		</p>
 		<p class="other">Examiner, lecturer and guest teacher at Westerdals Oslo ACT 2015–2019.</p>
 			<p>Board member of Grafill Interactive since 2014, arranging talks and workshops.</p>
